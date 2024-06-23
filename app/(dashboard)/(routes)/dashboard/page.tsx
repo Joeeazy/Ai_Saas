b53df40ff683325bc70de45e13e078ca478f01c3
Joeeazy <joeeazy34@gmail.com>
2024-06-23 04:39:35 +0300

    clerk/auth complete

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 export default function DashboardPage() {
-  return <div>dashboard page (protected)</div>;
+  return (
+    <div>
+      <p>dashboard page (protected)</p>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,19 @@
+import { Button } from "@/components/ui/button";
+import Link from "@/node_modules/next/link";
 import React from "react";
 
 export default function page() {
-  return <div>Landing Page Home Page(unprotected)</div>;
+  return (
+    <div>
+      Landing Page Home Page(unprotected)
+      <div>
+        <Link href="/sign-in">
+          <Button>Log In</Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>Register</Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
