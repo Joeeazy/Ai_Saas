0b8d1eb7e0003a8e51947e0ede40ef24cd1f7b7a
Joeeazy <joeeazy34@gmail.com>
2024-06-25 04:20:38 +0300

    Navbar and sidebar in progress

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-import React from "react";
-
 export default function DashboardPage() {
   return (
     <div>
       <p>dashboard page (protected)</p>
-      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
